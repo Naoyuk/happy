8d4dfe57e8eafc1e251ec08a9e3fb7db0db2e211
Naoyuki Ishida <39.ishida@gmail.com>
2020-12-27 11:26:19 -0800

    created post model and done post index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/show'
+  get 'posts/new'
+  post 'posts/create'
+  get 'posts/edit'
+  post 'posts/update'
+  delete 'posts/destroy'
+  
   get "/" => "home#top"
   get "/about" => "home#about"
 end
