1557a0321363e828ec0076c94e3cfe08eeff88fe
Naoyuki Ishida <39.ishida@gmail.com>
2021-01-11 15:20:25 -0800

    modified timezone setting to UTC on application config

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module Happy
   class Application < Rails::Application
+    config.time_zone = 'UTC'
+    config.active_record.default_timezone = :utc
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
